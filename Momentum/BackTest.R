1a3bf1ddc60725f2f33d3234c3e3436438b707b5
Victsz <wongtszngai@foxmail.com>
2014-07-22 13:56:31 +0800

    t

--- Momentum/BackTest.R
+++ Momentum/BackTest.R
@@ -1,5 +1,6 @@
+require(PerformanceAnalytics)
 #import data
-path<- 'RawData//000001.csv'
+path<- 'RawData//399006.csv'
 s<-getHistoryData(path, f ='%Y/%m/%d')
 #s<-s[100:200,]
 s$VOLUME<-NULL
@@ -56,8 +57,7 @@ for( i in 2:nrow(s) )
   isSell <- F
   isBuy  <- T
   curDate <- as.POSIXct(index(s)[i])
-  equity = getEndEq(myAcct, curDate)
-
+  equity = getEndEq(myAcct, curDate) 
   cl <- as.numeric(Cl(s[i,]))
   posn <- getPosQty(myPort, Symbol=Symbol, Date=curDate)
   
@@ -84,143 +84,60 @@ chart.Posn(myPort,Symbol,theme=myTheme)
 ts<-getTxns(Portfolio=myPort, Symbol=Symbol)
 View(ts)
 tstats <- tradeStats(Portfolio=myPort, Symbol=Symbol)
-View(tstats)
 
-
-
-entryPrice<-0
-isStoped<-F
-pices<-1
-dominator<-3
-bottom<-unique(s[s$dire==1,c('point')])
-#Iteration
-for( i in 1:nrow(s) )
-{
-  # update values for this date  
-  CurrentDate <- as.POSIXct(index(s)[i])
-  print(CurrentDate)
-  equity = getEndEq(myAcct, CurrentDate)
-  ClosePrice <- as.numeric(Cl(s[i,]))
-  OpenPrice <- as.numeric(Op(s[i,]))
-  Posn <- getPosQty(myPort, Symbol=Symbol, Date=CurrentDate)
-  
-  direN <- as.numeric(s[i,'dire'])
-  
-  # change market position if necessary
-  if(i>=2) # if the moving average has begun
-  {
-    direO <- as.numeric(s[i-1,'dire'])
-    if(direN==1 & direO == -1)
-    {
-      print('Up Trend')
-      if(Posn == 0 & isStoped)
-      {  
-        isStoped = F 
-      }
-    }
-    if(direN== -1 & direO ==1)
-    {
-      print('Down Trend')
-    }
-    
-  
-    if( Posn == 0 & !isStoped) 
-    { # No position, test to go Long
-      if( direN == 1 ) 
-      {
-        lastBottom <- coredata(Cl(s[max(bottom[bottom<i]),]))
-        if(as.numeric(s[i-1,'dire']) == -1){
-        cBottom <-  coredata(Cl(s[i,]))
-        if(lastBottom<cBottom)
-        {
-          pices = min(pices+1,dominator);
-          
-        }else{
-          
-          pices = 1;
-        }}
-        investMoney<-(equity*pices)/dominator
-        
-        if(investMoney > 0){
-        fee<-investMoney*0.01
-        
-        UnitSize = as.numeric(trunc(investMoney/ClosePrice))
-        addTxn(myPort, Symbol=Symbol, TxnDate=CurrentDate,
-               TxnPrice=ClosePrice, TxnQty = UnitSize , TxnFees=-fee, verbose =T) 
-        entryPrice<- ClosePrice}
-      }
-    }else if(Posn!=0){ # Have a position, so check exit
-      isExit<-F
-      if( direN == -1)
-      {
-        isExit = T
-        # exit position
-        lastBottom <- coredata(Cl(s[max(bottom[bottom<i]),]))
-        if(ClosePrice<lastBottom){
-       #   print('Former bottom stop')
-          isExit = T
-        }
-      } 
-#       peak<- s$peak[i]
-#       if(ClosePrice< peak*(1-0.015)){
-#         #stop Lose
-#         print('Drawback stop')
-#         isExit = T
-#       } else {
-#         if(ClosePrice> entryPrice*(1+0.08)){
-#           #stop Lose
-#           print('Take profit')
-#           isExit = T
-#         }else if(coredata(Op(s[i,])) < (coredata(Cl(s[i-1,]))*(1-0.003))){
-#           if(ClosePrice< coredata(Cl(s[i-1,]))){
-#             isExit = T
-#             print('Gap Stop')}
-#           
-#         }
-#       }
-      
-      if(isExit){
-        isStoped = T
-        addTxn(myPort, Symbol=Symbol, TxnDate=CurrentDate,
-               TxnPrice=ClosePrice, TxnQty = -Posn , TxnFees=0, verbose =T  ) 
-        isExit = F
-      }
-    }
-    
-   
-    
-    
+ tab.trades <- cbind(
+   c("Trades","Win Percent","Loss Percent","W/L Ratio"),
+   c(tstats[,"Num.Trades"],tstats[,c("Percent.Positive","Percent.Negative")],
+     tstats[,"Percent.Positive"]/tstats[,"Percent.Negative"]))
  
-    # Calculate P&L and resulting equity with blotter
-    
-    updatePortf(myPort,Dates = CurrentDate)
-    updateAcct(myAcct, Dates = CurrentDate)
-    updateEndEq(myAcct, Dates = CurrentDate)
-  } # End dates loop
-}
+ tab.profit <- cbind(
+   c("Net Profit","Gross Profits","Gross Losses","Profit Factor"),
+   c(tstats[,c("Net.Trading.PL","Gross.Profits","Gross.Losses",
+               "Profit.Factor")]))
 
-chart.Posn(myPort,Symbol,theme=myTheme)
+ tab.wins <- cbind(
+   c("Avg Trade","Avg Win","Avg Loss","Avg W/L Ratio"),
+   c(tstats[,c("Avg.Trade.PL","Avg.Win.Trade","Avg.Losing.Trade",
+               "Avg.WinLoss.Ratio")]))
 
-ts<-getTxns(Portfolio=myPort, Symbol=Symbol)
-View(ts)
-tstats <- tradeStats(Portfolio=myPort, Symbol=Symbol)
+trade.stats.tab <- data.frame(tab.trades,tab.profit,tab.wins)
+View(trade.stats.tab)
 
 
-# tab.trades <- cbind(
-#   c("Trades","Win Percent","Loss Percent","W/L Ratio"),
-#   c(tstats[,"Num.Trades"],tstats[,c("Percent.Positive","Percent.Negative")],
-#     tstats[,"Percent.Positive"]/tstats[,"Percent.Negative"]))
-# 
-# tab.profit <- cbind(
-#   c("Net Profit","Gross Profits","Gross Losses","Profit Factor"),
-#   c(tstats[,c("Net.Trading.PL","Gross.Profits","Gross.Losses",
-#               "Profit.Factor")]))
-# 
-# tab.wins <- cbind(
-#   c("Avg Trade","Avg Win","Avg Loss","Avg W/L Ratio"),
-#   c(tstats[,c("Avg.Trade.PL","Avg.Win.Trade","Avg.Losing.Trade",
-#               "Avg.WinLoss.Ratio")]))
-# trade.stats.tab <- data.frame(tab.trades,tab.profit,tab.wins)
+rets <- PortfReturns(Account=myAcct)
+rownames(rets) <- NULL
+tail(rets)
+charts.PerformanceSummary(rets,colorset = bluefocus)
+tab.perf <- table.Arbitrary(rets,
+                            metrics=c(
+                              "Return.cumulative",
+                              "Return.annualized",
+                              "SharpeRatio.annualized",
+                              "CalmarRatio"),
+                            metricsNames=c(
+                              "Cumulative Return",
+                              "Annualized Return",
+                              "Annualized Sharpe Ratio",
+                              "Calmar Ratio"))
+tab.perf 
+
+tab.risk <- table.Arbitrary(rets,
+                            metrics=c(
+                              "StdDev.annualized",
+                              "maxDrawdown",
+                              "VaR",
+                              "ES"),
+                            metricsNames=c(
+                              "Annualized StdDev",
+                              "Max DrawDown",
+                              "Value-at-Risk",
+                              "Conditional VaR"))
+tab.risk
+
+performance.stats.tab <- data.frame(
+  rownames(tab.perf),tab.perf[,1],
+  rownames(tab.risk),tab.risk[,1])
+View(performance.stats.tab)
 # 
 # require(PerformanceAnalytics)
 # rets <- PortfReturns(Account = myAcct)
